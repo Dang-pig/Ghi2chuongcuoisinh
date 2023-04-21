3d7458f11048a5d3adcb54fd525451fd412f1814
Dang-pig <85467685+Dang-pig@users.noreply.github.com>
2023-04-21 23:13:14 +0700

    Create godiem.py

---
+++ godiem.py
@@ -0,0 +1,7 @@
+canGoDiem = int(input("Mày có cần gỡ điểm ko? (1/0)")) #cần gỡ điểm
+while(canGoDiem != 0 and canGoDiem != 1):
+  canGoDiem = int(input("Mày có cần gỡ điểm ko? (1/0)"))
+if canGoDiem:
+  print("Ghi full tiêu đề và ghi nhớ 2 chương cuối")
+else:
+  print("Ghi full tiêu đề 2 chương cuối");
