4dbc57132edb438ab2cf10b1aa06f9ce2c60401b
Dang-pig <85467685+Dang-pig@users.noreply.github.com>
2023-04-21 23:12:58 +0700

    Create godiem.js

---
+++ godiem.js
@@ -0,0 +1,4 @@
+let canGoDiem = prompt("Mày có cần gỡ điểm ko? (1/0)"); //cần gỡ điểm
+while(canGoDiem != 0 && canGoDiem != 1) let canGoDiem = prompt("Mày có cần gỡ điểm ko?");
+if(canGoDiem) alert("Ghi full tiêu đề và ghi nhớ 2 chương cuối");
+else alert("Ghi full tiêu đề 2 chương cuối");
